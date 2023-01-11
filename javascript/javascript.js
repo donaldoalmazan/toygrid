6e584dd48a57915ddfd55305c5b4c71ae4bcccf0
Steve Traugott <stevegt@t7a.org>
2023-01-11 06:14:00 -0800

    add short timeout to javascript version for reference

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,3 +1,8 @@
 // simple hellow world javascript demo that replaces the content of the div with id "javascript" with "Hello World!"
-document.getElementById("javascript").innerHTML = "Hello World!";
+setTimeout(function(){
+    document.getElementById("javascript").innerHTML = "Hello World!";
+    console.log("javascript.js said hello");
+}, 100);
+
+console.log("javascript.js loaded");
 
